92040c90f5f1cee46196f9b3a651c3742d215a12
Juyoung Kim <juyoungk@stanford.edu>
2017-10-26 15:38:48 -0700

    test

---
+++ push_git.m
@@ -0,0 +1,26 @@
+function push_git(varargin)
+
+p = ParseInput(varargin{:});
+commit_msg = p.Results.comment;
+
+eval('!git add -A')
+eval(['!git commit -am ', commit_msg])
+eval('!git push')
+
+end
+
+function p =  ParseInput(varargin)
+    
+    p  = inputParser;   % Create an instance of the inputParser class.
+    
+    addParamValue(p,'comment', 'my Macbook pro');
+    
+%     addParamValue(p,'barWidth', 100, @(x)x>=0);
+%     addParamValue(p,'barSpeed', 1.4, @(x)x>=0);
+%     addParamValue(p,'barColor', 'dark', @(x) strcmp(x,'dark') || ...
+%         strcmp(x,'white'));
+%      
+    % Call the parse method of the object to read and validate each argument in the schema:
+    p.parse(varargin{:});
+    
+end
